daefc1325b3c90041e5af0d86e7c481b34b0a7cd
ink <scenic.wow@gmail.com>
2016-12-12 09:28:49 +0800

    qt widgets opengl example

---
+++ OpenGLWindow/mainwindow.cpp
@@ -0,0 +1,36 @@
+﻿#include "mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+{
+  setSurfaceType(QWindow::OpenGLSurface);
+
+}
+MainWindow::~MainWindow()
+{
+
+}
+void MainWindow::initializeGL()
+{
+
+}
+
+void MainWindow::resizeGL(int w, int h)
+{
+
+}
+
+
+void MainWindow::paintGL()
+{
+
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+
+}
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+
+}
---
+++ OpenGLWindow/mainwindow.h
@@ -0,0 +1,23 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+#include <QOpenGLWindow>
+class MainWindow : public QOpenGLWindow
+{
+  Q_OBJECT
+  public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+  protected:
+    virtual void initializeGL();
+    virtual void resizeGL(int w, int h);
+    virtual void paintGL();
+    void paintEvent(QPaintEvent *event);
+    void resizeEvent(QResizeEvent *event);
+};
+
+
+#endif // MAINWINDOW_H
+
+
